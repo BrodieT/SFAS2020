db9b432d304f68d4948bf7e6d9608bc9ef831d18
DESKTOP-MS8EKL2\Brodie Templeton <1601797@uad.ac.uk>
2019-12-19 00:17:40 +0000

    Prototype for randomly generating structures on the map, altered the materials on tile prefabs, changed menu structure

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    bool gameStart = false;
+    public GameObject player = new GameObject();
+    Vector3 Direction = new Vector3();
+    // Update is called once per frame
+    void Update()
+    {
+        if(!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            gameStart = true;
+        }
+
+        if (gameStart)
+        {
+           
+
+        }
+    }
+}
